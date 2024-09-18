e07bf06d9f945ffe669625e4eafea94223455cc6
frantisektomas <wfrantisektomas@gmail.com>
2024-09-18 17:35:38 +0200

    created brek time

--- pomodoro.py
+++ pomodoro.py
@@ -1,15 +1,25 @@
 import time
 
-duration = int(input())
+pomo_duration = int(input("how long do you want do pomodoro : ")) * 60
+break_duration = int(input("and how long do you want the break to be : ")) * 60
 
-for count in range(duration, 0, -1):
+for count in range(pomo_duration, 0, -1):
 
     seconds = count % 60
     minutes = int(count / 60) % 60
-    hours = int(count / 3600)
 
-    print(f"\r{hours:02}:{minutes:02}:{seconds:02}", end='')
+    print(f"\r{minutes:02}:{seconds:02}", end='')
 
     time.sleep(1)
 
-print("\nend")
+for count in range(break_duration, 0, -1):
+
+    seconds = count % 60
+    minutes = int(count / 60) % 60
+
+    print(f"\r{minutes:02}:{seconds:02}", end='')
+
+    time.sleep(1)
+
+
+print("\npomodoro and break are over, restart and go study again bitch")
